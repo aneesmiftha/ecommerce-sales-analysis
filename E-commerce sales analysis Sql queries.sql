3f318183b58adf3d8adbe4ccf6fff594343f98a3
aneesmiftha <mifthaanees@gmail.com>
2026-05-01 22:21:33 +0530

    Update E-commerce sales analysis Sql queries.sql

--- E-commerce sales analysis Sql queries.sql
+++ E-commerce sales analysis Sql queries.sql
@@ -1,5 +1,24 @@
-SELECT City, 
-round(SUM(Total_Sales)/SUM(Quantity), 2) as avg_order_value
-from ecommerce
-group by City
-order by avg_order_value desc;
+SELECT Product, SUM(Quantity) AS total_qty
+FROM ecommerce
+GROUP BY Product
+ORDER BY total_qty DESC;
+
+SELECT Product, SUM(Total_Sales) AS revenue
+FROM ecommerce
+GROUP BY Product
+ORDER BY revenue DESC;
+
+SELECT City, SUM(Total_Sales) AS revenue
+FROM ecommerce
+GROUP BY City
+ORDER BY revenue DESC;
+
+SELECT MONTH(Order_Date) AS month, SUM(Total_Sales) AS revenue
+FROM ecommerce
+GROUP BY month;
+
+SELECT City,
+       ROUND(SUM(Total_Sales)/SUM(Quantity),2) AS avg_order_value
+FROM ecommerce
+GROUP BY City
+ORDER BY avg_order_value DESC;
